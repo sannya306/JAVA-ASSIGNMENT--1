432a3a404e94ca9550b5cee54ea6b30adc4bfb92
sannya306 <neepan14@gmail.com>
2026-08-18 21:27:52 +0530

    Create Fahrenheit.java

---
+++ Fahrenheit.java
@@ -0,0 +1,11 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+
+        int celsius = 25;
+
+        double fahrenheit = (celsius * 9.0 / 5) + 32;
+
+        System.out.println("Temperature in Celsius: " + celsius);
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
