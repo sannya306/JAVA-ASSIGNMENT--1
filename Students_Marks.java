cc82cd41ad72bd5e4e8517eae16464dabf07bbc2
sannya306 <neepan14@gmail.com>
2026-08-18 21:37:49 +0530

    Add StudentMarks class to input and display marks

---
+++ Students_Marks.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StudentMarks {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] marks = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter marks of student " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        System.out.println("\nMarks of students:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Student " + (i + 1) + ": " + marks[i]);
+        }
+    }
+}
