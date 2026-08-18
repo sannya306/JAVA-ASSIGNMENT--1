08c118ad891d28601e1306ac66af301ccc7749f4
sannya306 <neepan14@gmail.com>
2026-08-18 21:32:14 +0530

    Add SimpleCalculator for basic arithmetic operations

---
+++ Multiplication_Table.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        if (operator == '+') {
+            System.out.println("Result: " + (num1 + num2));
+        } 
+        else if (operator == '-') {
+            System.out.println("Result: " + (num1 - num2));
+        } 
+        else if (operator == '*') {
+            System.out.println("Result: " + (num1 * num2));
+        } 
+        else if (operator == '/') {
+            System.out.println("Result: " + (num1 / num2));
+        } 
+        else {
+            System.out.println("Invalid operator.");
+        }
+    }
+}
