55f29f20d415c48b95c5ec2bd02e8433818aa1ba
sannya306 <neepan14@gmail.com>
2026-08-18 21:23:45 +0530

    Add StudentDetails class to display student info

---
+++ Eligibility.java
@@ -0,0 +1,16 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        String studentName = "Sanya";
+        int rollNumber = 101;
+        int subject1Marks = 85;
+        int subject2Marks = 90;
+        int subject3Marks = 88;
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Subject 1 Marks: " + subject1Marks);
+        System.out.println("Subject 2 Marks: " + subject2Marks);
+        System.out.println("Subject 3 Marks: " + subject3Marks);
+    }
+}
